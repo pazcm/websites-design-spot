bde86d7391cef1c134049c520ea22cbeeae1ebd0
Marta <marta@deveire.com>
2019-10-14 14:17:17 +0100

    add logout functionality

--- websites_design_spot/websites_design_spot/urls.py
+++ websites_design_spot/websites_design_spot/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from account.views import index
+from account.views import index, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index)
+    url(r'^$', index, name="index"),
+    url(r'^account/logout/$', logout, name="logout")
 ]
