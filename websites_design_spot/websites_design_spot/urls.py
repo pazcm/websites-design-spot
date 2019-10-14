30a881a4e72af40d5cda5e075af8364e7398597b
Marta <marta@deveire.com>
2019-10-14 14:29:29 +0100

    add login template and view

--- websites_design_spot/websites_design_spot/urls.py
+++ websites_design_spot/websites_design_spot/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from account.views import index, logout
+from account.views import index, logout, login
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name="index"),
-    url(r'^account/logout/$', logout, name="logout")
+    url(r'^account/logout/$', logout, name="logout"),
+    url(r'^account/login/$', login, name="login")
 ]
