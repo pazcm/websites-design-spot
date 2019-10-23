fb0f4dee071e2a5e5d33c05db4ffd2938a6911c4
Marta <marta@deveire.com>
2019-10-23 16:19:53 +0100

    create custom storage file to split the S3 bucket (media and static)

---
+++ websites_design_spot/custom_storages.py
@@ -0,0 +1,11 @@
+# Split the bucket to havestatic content in one directory in S3 and media(images, ...) in other
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
--- websites_design_spot/websites_design_spot/settings.py
+++ websites_design_spot/websites_design_spot/settings.py
@@ -155,8 +155,9 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-# define storages as static file
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# class in custom_storages.py
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 AWS_DEFAULT_ACL = None
 
@@ -164,6 +165,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
