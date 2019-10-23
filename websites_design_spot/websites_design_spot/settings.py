1e78e3ee2ad353cbdbceedcba8c78ffa78b362aa
Marta <marta@deveire.com>
2019-10-23 17:02:09 +0100

    change settings DB conexion for travis

--- websites_design_spot/websites_design_spot/settings.py
+++ websites_design_spot/websites_design_spot/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
+# import env
 import dj_database_url
 import psycopg2
 
@@ -92,12 +92,17 @@ WSGI_APPLICATION = 'websites_design_spot.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'))}
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
