4784473c58186eb742beec33e673de2b8e559552
Marta <marta@deveire.com>
2019-11-27 15:45:19 +0000

    update getintouch form

--- websites_design_spot/getintouch/forms.py
+++ websites_design_spot/getintouch/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class GetInTouchForm(forms.Form):
     contact_name = forms.CharField(required=True, label="First Name")
-    contact_lname= forms.CharField(required=True, label="Last Name")
+    contact_surname= forms.CharField(required=True, label="Last Name")
     from_email = forms.EmailField(required=True, label="Email")
     contact_website = forms.CharField(required=True, label="Website")
     contact_company = forms.CharField(required=True, label="Company")
