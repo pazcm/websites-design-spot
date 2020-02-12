0a456b9df70caa3d11ac76d640241aa24a21ea9c
Marta <marta@deveire.com>
2020-02-12 12:22:38 +0000

    add Django testCase webdes

--- webdes/tests.py
+++ webdes/tests.py
@@ -1,13 +1,14 @@
 from django.test import TestCase
 from .models import Webdes
 
-# Create your tests here.
-
 class WebdesTests(TestCase):
-    """
-    Here is defined an initial test that will run against the Webdes model
-    """
+    def setUp(self):
+        Webdes.objects.create(name="Eco Mug Product Website", category="Event")
+        Webdes.objects.create(name="Glasses Collection Website", category="Professional")
 
-    def test_str(self):
-        test_name = Webdes(name='A web design')
-        self.assertEqual(str(test_name), 'A web design')
+    def test_webdes_has_category(self):
+        """Webdes that have category are correctly selected"""
+        mugWeb = Webdes.objects.get(name="Eco Mug Product Website")
+        glassesWeb = Webdes.objects.get(name="Glasses Collection Website")
+        self.assertEqual(mugWeb.has(), 'This design has category "Event"')
+        self.assertEqual(glassesWeb.has(), 'This design has category "Professional"')
