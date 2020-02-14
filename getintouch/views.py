bc34b7dd6b6629f1ef3d5a89a79921f28c6b9359
Marta <marta@deveire.com>
2020-02-14 12:32:46 +0000

    add success messages python contact form

--- getintouch/views.py
+++ getintouch/views.py
@@ -3,6 +3,8 @@ from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .forms import GetInTouchForm
+from django.contrib import messages
+from django.core.urlresolvers import reverse
 
 def contactView(request):
     if request.method == 'GET':
@@ -17,7 +19,9 @@ def contactView(request):
                 send_mail(message, from_email, contact_name, ['admin@example.com'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
+            messages.success(request, "Your message has been sent successfully!")    
             return redirect('success')
+            
     return render(request, "contact.html", {'form': form})
 
 def successView(request):
